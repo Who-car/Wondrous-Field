4f6c68630b77df43e30032338e9181122a4ab5ec
NikoImam <niyaz120904@gmail.com>
2023-12-07 23:05:35 +0300

    fix: added -GetContent- function

--- PackageHelper/Package.cs
+++ PackageHelper/Package.cs
@@ -2,15 +2,15 @@
 {
     public static class Package
     {
+        public const int ReservedSize = 15;
         public const int MaxPackageSize = 256;
-        public const int MaxContentSize = 240;
-        public const int MaxFreeBytes = MaxPackageSize - MaxContentSize;
+        public const int MaxBodySize = MaxPackageSize - ReservedSize;
 
         public const byte Separator = 0x2F;
         public const byte End = 0x0;
-        public static readonly (int, int) Command = (6, 9);
         public const int Fullness = 11;
         public const int Query = 12;
+        public const int BodyStartIndex = ReservedSize;
 
         //Protocol: #ANTP/****/**/...[END]
 
@@ -19,9 +19,9 @@
             0x23, 0x41, 0x4E, 0x50, Separator
         };
 
-        public static byte[] GetContent(byte[] buffer, int contentLength)
+        public static byte[] GetContent(byte[] package, int contentLength)
         {
-            return default!;
+            return package.Take(new Range(BodyStartIndex, contentLength - 2)).ToArray();
         }
         
         public static byte[] CreatePackage(byte[] content)
