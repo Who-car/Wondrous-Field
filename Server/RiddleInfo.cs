61c6e8241ebac0ce1784a0e84453f1733f841f87
NikoImam <niyaz120904@gmail.com>
2023-12-15 00:17:08 +0300

    fix: added moving to next player

---
+++ Server/RiddleInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class RiddleInfo
+    {
+        public string Riddle { get; set; }
+        public string Word { get; set; }
+    }
+}
